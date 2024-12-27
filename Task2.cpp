b92461260abc72b2bbd3420ad0d852231b765663
KIMJH424 <kim2005kgy@gmail.com>
2024-12-27 14:49:49 +0900

    Task_2
    과제 2번 필수과제 , 도전과제

---
+++ Task2.cpp
@@ -0,0 +1,93 @@
+#include<iostream>
+#include<cstdlib>
+#include<ctime>
+
+using namespace std;
+
+class Animal {
+public:
+	virtual void makesound() = 0;
+	virtual void move() = 0;
+	virtual ~Animal() {}
+};
+
+class Dog : public Animal {
+public:
+	void makesound() override {
+		cout << "Woof Woof!" << endl;
+	}
+	void move() override {
+		cout << "The Dog is wagging its tail." << endl;
+	}
+};
+
+class Cat : public Animal {
+public:
+	void makesound() override {
+		cout << "meowwww" << endl;
+	}
+	void move() override {
+		cout << "The cat is meowing" << endl;
+	}
+};
+
+class Cow : public Animal {
+public:
+	void makesound() override {
+		cout << "Mooooooo~" << endl;
+	}
+	void move() override {
+		cout << "The cow is chewing cud" << endl;
+	}
+};
+
+Animal* createRandomAnimal(){
+	int randomValue = rand() % 3;
+
+	if (randomValue == 0){
+		return new Dog();
+    }
+	else if (randomValue == 1) {
+		return new Cat();
+	}
+	else {
+		return new Cow();
+	}
+}
+class Zoo {
+private :
+	Animal* animals[10];
+	int animalCount;
+public :
+	Zoo() : animalCount(0){}
+	void addAnimal(Animal* animal) {
+		if (animalCount < 10) {
+			animals[animalCount++] = animal;
+		}
+	}
+
+	void performActions() {
+		for (int i = 0;i < animalCount;++i) {
+			animals[i]->makesound();
+			animals[i]->move();
+		}
+	}
+
+	~Zoo() {
+		for (int i = 0;i < animalCount;++i) {
+			delete animals[i];
+		}
+	}
+		
+	
+};
+
+int main() {
+	srand(static_cast<unsigned int>(time(0)));
+	Zoo zoo;
+	for (int i = 0;i < 10;++i) {
+		zoo.addAnimal(createRandomAnimal());
+	}
+	zoo.performActions();
+	return 0;
+}
